3e8e897583745bd9fc21313774e0a062ee3ca18a
luishrs <luis-hrs@outlook.com>
2023-10-11 18:45:06 -0300

    Feat:added models to the database

---
+++ app/Models/Adress.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Adress extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public contry: string
+
+  @column()
+  public state: string
+
+  @column()
+  public city: string
+
+  @column()
+  public neighborhood: string
+
+  @column()
+  public street: string
+
+  @column()
+  public number: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
