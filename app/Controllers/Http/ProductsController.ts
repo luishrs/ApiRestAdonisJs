52bb01fd452392bde04bbdd2e98699b434243e38
luishrs <luis-hrs@outlook.com>
2023-10-14 16:17:54 -0300

    Fix: Refactoring code

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -5,6 +5,7 @@ import ProductValidator from 'App/validations/producValidator'
 export default class ProductsController {
   public async store ({ request, response }: HttpContextContract) {
 const body = request.body()
+const { name} = body
 
   try {
       await request.validate(ProductValidator)
@@ -12,17 +13,22 @@ const body = request.body()
       return response.status(400).json({erro: errors[0].message})
     }
 
-  try {
+    const productExists = await Product.findBy('name', name)
+    if (productExists?.name === name) {
+      return response.status(400).json({message: 'Product already exists'})
+    }
+
+    try {
     const product = await Product.create(body)
     return response.status(201).json(product)
-  }    catch (error) {
+    }    catch (error) {
       return response.status(400).json({message: error.message})
     }
   }
 
   public async index ({ response }: HttpContextContract) {
     try {
-      const products = await Product.all()
+      const products = await Product.query().orderBy('id', 'desc')
       return response.status(200).json(products)
     } catch (error) {
       return response.status(400).json({message: error.message})
