e025116fbd60d01b9d494a34c024222e8124d782
luishrs <luis-hrs@outlook.com>
2023-10-14 13:33:10 -0300

    Fix: fixed update method errors

--- app/Controllers/Http/ClientsController.ts
+++ app/Controllers/Http/ClientsController.ts
@@ -1,6 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Client from 'App/Models/Client'
-import ClientUpdateValidator from 'App/validations/clientValidator'
+import ClientUpdateValidator from 'App/validations/clientUpdateValidator'
 import ClientValidator from 'App/validations/clientValidator'
 
 export default class ClientsController {
