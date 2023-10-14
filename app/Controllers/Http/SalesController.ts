0d10ccc67083512c3af111f377d7c8fd0831c45b
luishrs <luis-hrs@outlook.com>
2023-10-14 16:36:07 -0300

    Fix:refactoring salesController code

--- app/Controllers/Http/SalesController.ts
+++ app/Controllers/Http/SalesController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Product from 'App/Models/Product'
 import Sale from 'App/Models/Sale'
 import SaleValidator from 'App/validations/saleValidator'
 import SaleShowValidator from 'App/validations/salesShowValidator'
@@ -6,15 +7,24 @@ import SaleShowValidator from 'App/validations/salesShowValidator'
 export default class SalesController {
   public async store ( {request, response}: HttpContextContract) {
     const body = request.body()
+    const { client_id, product_id, quantity,} = body
     try {
       await request.validate(SaleValidator)
     } catch ({messages: {errors}}) {
       return response.status(400).json({erro: errors[0].message})
     }
 
+    const product = await Product.findBy('id', product_id)
+    const sale = {
+      client_id,
+      product_id,
+      quantity,
+      unit_price: Number(product?.price),
+    }
+
     try {
-      const sale = await Sale.create(body)
-      return response.status(201).json(sale)
+      const saleregistred = await Sale.create(sale)
+      return response.status(201).json(saleregistred)
     } catch (error) {
       return response.status(400).json({message: error.message})
     }     
